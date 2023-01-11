9e357487b81c7e45a471b060b6a3ffd4c4a3ea51
A_Bruce <96545712+RUTAGENGWA-ASANTE-BRUCE@users.noreply.github.com>
2023-01-11 13:47:33 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,68 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Circle, Svg, Path } from 'react-native-svg';
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import tw from 'twrnc';
+import RelaxationTemplate from './components/RelaxationTemplate';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { Flower } from './utils/Icons';
+/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
+ * LTI update could not be added via codemod */
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './screens/LoginScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import HowAreYouFeelingScreen from './screens/HowAreYouFeelingScreen';
+const Stack = createStackNavigator();
+const App = () => {
+
+  return (
+    <SafeAreaView style={{ flex: 1, margin: 0, padding: 0 }}>
+
+      <NavigationContainer>
+
+        <Stack.Navigator>
+          <Stack.Screen
+            name="HowAreYouFeeling"
+            component={HowAreYouFeelingScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="LoginScreen"
+            component={LoginScreen}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
---
+++ src/components/RelaxationTemplate.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+import tw from 'twrnc';
+const RelaxationTemplate = () => {
+    return (
+        <View>
+            <Text style={tw``}>Relaxe</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({})
+
+export default RelaxationTemplate;
